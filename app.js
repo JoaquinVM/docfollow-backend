d3f20f39656fc8f96cfa93cb74ff00e75b0c47c6
joaquinviscafe <viscafe38@gmail.com>
2020-01-07 18:08:59 -0400

    CORS conf

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const docente_routes = require('./routes/docente');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.use('/', materia_routes);
-app.use('/', docente_routes);
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -18,5 +15,9 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
+
+app.use('/', materia_routes);
+app.use('/', docente_routes);
+
 
 module.exports = app;
