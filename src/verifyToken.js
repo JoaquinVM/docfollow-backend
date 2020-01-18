9726ee34410a7fafbf6fb8de1f5cd94ce3d06dd6
joaquinviscafe <viscafe38@gmail.com>
2020-01-18 19:47:57 -0400

    bug fixes

--- src/verifyToken.js
+++ src/verifyToken.js
@@ -5,29 +5,35 @@ const response = require('./utils').response;
 const client = new OAuth2Client('602723697704-ucdbgn6m678gf5rkj02npjl2rrcak250.apps.googleusercontent.com');
 var userInfo;
 
-async function getUserInfo(token) {
+async function verify(token) {
     const ticket = await client.verifyIdToken({
         idToken: token,
         audience: '602723697704-ucdbgn6m678gf5rkj02npjl2rrcak250.apps.googleusercontent.com',
     });
     userInfo = ticket.getPayload();
-
 }
 
 function TokenValidation(req, res, next) {
     const token = req.header('Token');
     if(!token) return res.status(401).send({ message: 'Acceso denegado'});
 
-    Usuario.findOne({email: userInfo['email']}).exec((err, user) => {
-        if(err) return res.status(500).send({
-            message: 'Error de conexion a la base de datos'
-        });
-        if(!user) return res.status(404).send({
-            message: 'Esta cuenta no existe'
+    verify(token)
+        .then( () => {
+            Usuario.findOne({email: userInfo['email']}).exec((err, user) => {
+                if(err) return res.status(500).send({
+                    message: 'Error de conexion a la base de datos'
+                });
+                if(!user) return res.status(404).send({
+                    message: 'Esta cuenta no existe'
+                });
+
+                next();
+            });
+        })
+        .catch(err => {
+            res.status(500).send({message: "Error al iniciar sesion"})
         });
 
-        next();
-    });
 }
 
 module.exports = TokenValidation;
